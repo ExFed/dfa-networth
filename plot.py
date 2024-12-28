513ac9ffe121cb12ce847808e4c0f033e142933a
David Allen <david.allen@columnzero.com>
2024-12-27 23:43:10 -0500

    Add president names

--- plot.py
+++ plot.py
@@ -2,6 +2,8 @@
 
 from io import StringIO
 import datetime as dt
+from matplotlib.dates import date2num
+from matplotlib.patheffects import Normal, SimpleLineShadow
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
@@ -44,6 +46,39 @@ def get_first_color(colors):
     return colors.split(";")[0]
 
 
+def plot_presidents(presidents, ax=plt.gca()):
+    presidents["color"] = presidents["partyColors"].apply(get_first_color)
+    left, right = ax.get_xlim()
+
+    for president in presidents.itertuples():
+        name = president.presidentLabel
+        start = dt.datetime.strptime(president.start, "%Y-%m-%dT%H:%M:%SZ")
+        end = dt.datetime.strptime(president.end, "%Y-%m-%dT%H:%M:%SZ")
+        color = f"#{president.color}"
+        plt.axvspan(start, end, alpha=0.15, color=color)
+
+        num_start = date2num(start)
+        num_end = date2num(end)
+        x_start = (num_start - left) / (right - left)
+        x_end = (num_end - left) / (right - left)
+        if x_start > 0 and x_start < 1 or x_end > 0 and x_end < 1:
+            x = max(0, x_start) + 0.01
+            plt.text(
+                x,
+                0.99,
+                name,
+                rotation=90,
+                transform=ax.transAxes,
+                ha="left",
+                va="top",
+                fontsize=9,
+                path_effects=[
+                    SimpleLineShadow((1, -1), shadow_color="gray", linewidth=1),
+                    Normal(),
+                ],
+            )
+
+
 def get_networth_levels(cache_file) -> pd.DataFrame:
     if os.path.isfile(cache_file):
         return pd.read_csv(cache_file)
@@ -66,24 +101,7 @@ def parse_yearquarter(yq: str):
     return dt.datetime(int(y), m, d)
 
 
-def main():
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-    data_dir = os.path.join(script_dir, "data")
-    os.makedirs(data_dir, exist_ok=True)
-
-    presidents = get_usa_presidents(os.path.join(data_dir, "usa-presidents.csv"))
-    presidents["color"] = presidents["partyColors"].apply(get_first_color)
-
-    for president in presidents.itertuples():
-        name = president.presidentLabel
-        start = dt.datetime.strptime(president.start, "%Y-%m-%dT%H:%M:%SZ")
-        end = dt.datetime.strptime(president.end, "%Y-%m-%dT%H:%M:%SZ")
-        color = president.color
-        print(f"{name} ({color}) => {start} - {end}")
-        plt.axvspan(start, end, alpha=0.25, color=f"#{color}")
-
-    networth = get_networth_levels(os.path.join(data_dir, "dfa-networth-levels.csv"))
-
+def plot_networth(networth):
     networth["Date"] = networth["Date"].apply(parse_yearquarter)
     grouped = networth.groupby("Category")
 
@@ -99,12 +117,27 @@ def main():
 
     # Plot stacked area plot
     plt.stackplot(stack_data.index, stack_data.T, labels=categories)
+    plt.xlim(dates[0], dates[-1])
+
+
+def main():
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    data_dir = os.path.join(script_dir, "data")
+    os.makedirs(data_dir, exist_ok=True)
+
+    presidents = get_usa_presidents(os.path.join(data_dir, "usa-presidents.csv"))
+    networth = get_networth_levels(os.path.join(data_dir, "dfa-networth-levels.csv"))
+
+    plot_networth(networth)
+    plot_presidents(presidents)
+
     plt.xlabel("Date")
     plt.xticks(rotation=45, ha="right")
     plt.ylabel("Net worth")
     plt.yscale("log")
     plt.title("Net Worth vs Date by Wealth Percentile")
-    plt.legend()
+    plt.subplots_adjust(right=0.7)
+    plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
     plt.show()
 
 
